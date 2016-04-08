a6b1e571f7c531f8dbda9c1d95e9104461330eea
Simon Baynes <baynezy@gmail.com>
2016-04-08 06:30:44 +0100

    Parking what I have

---
+++ src/GitHubLabelInitialiser.Web/Helpers/GitHubLoginHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace GitHubLabelInitialiser.Web.Helpers
+{
+	public class GitHubLoginHelper
+	{
+		private readonly string _baseUrl;
+
+		public GitHubLoginHelper(string baseUrl)
+		{
+			_baseUrl = baseUrl;
+		}
+
+		public string LoginButton(string clientId)
+		{
+			return string.Format(@"<a href=""{0}?client_id={1}"">Create account with GitHub</a>", _baseUrl, clientId);
+		}
+	}
+}
---
+++ test/GitHubLabelInitialiser.Web.Test/Helpers/GitHubLoginHelperTest.cs
@@ -0,0 +1,32 @@
+﻿using GitHubLabelInitialiser.Web.Helpers;
+using NUnit.Framework;
+
+namespace GitHubLabelInitialiser.Web.Test.Helpers
+{
+	[TestFixture]
+	class GitHubLoginHelperTest
+	{
+		private static string baseURL = "http://localhost/";
+		[Test]
+		public void LoginButton_WhenPassingInClientIdOnly_ThenReturnButtonHtml()
+		{
+			const string clientId = "wereteWHDSAWETW";
+			var helper = CreateHelper();
+			var button = helper.LoginButton(clientId);
+
+			AssertLoginButton(button, clientId);
+		}
+
+		private static GitHubLoginHelper CreateHelper()
+		{
+			return new GitHubLoginHelper(baseURL);
+		}
+
+		private static void AssertLoginButton(string button, string clientId)
+		{
+			var constructedButton = @"<a href=""" + baseURL + "?client_id=" + clientId + @""">Create account with GitHub</a>";
+
+			Assert.That(button, Is.EqualTo(constructedButton));
+		}
+	}
+}
