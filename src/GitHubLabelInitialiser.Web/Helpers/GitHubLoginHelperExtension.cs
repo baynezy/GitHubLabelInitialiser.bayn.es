0d7327b2de634cb0618d82c137a594af1ef6c532
Simon Baynes <baynezy@gmail.com>
2016-04-20 18:12:50 +0100

    Extended GitLogin HtmlHelper to allow the passing in of the state to allow it to be used in other calls. #9

--- src/GitHubLabelInitialiser.Web/Helpers/GitHubLoginHelperExtension.cs
+++ src/GitHubLabelInitialiser.Web/Helpers/GitHubLoginHelperExtension.cs
@@ -8,25 +8,25 @@ namespace GitHubLabelInitialiser.Web.Helpers
 	{
 		private const string BaseUrl = "https://github.com/login/oauth/authorize";
 
-		public static MvcHtmlString LoginButton(this HtmlHelper htmlHelper, string clientId, string redirectUri = null, IList<string> scope = null)
+		public static MvcHtmlString LoginButton(this HtmlHelper htmlHelper, string clientId, string redirectUri = null, IList<string> scope = null, string state = null)
 		{
 			var link = new TagBuilder("a");
-			link.MergeAttribute("href", CreateLink(clientId, redirectUri, scope));
+			link.MergeAttribute("href", CreateLink(clientId, state ?? System.Guid.NewGuid().ToString(), redirectUri, scope));
 			link.InnerHtml = "Create account with GitHub";
 			var html = link.ToString();
 
 			return MvcHtmlString.Create(html);
 		}
 
-		private static string CreateLink(string clientId, string redirectUri = null, IEnumerable<string> scope = null)
+		private static string CreateLink(string clientId, string state, string redirectUri = null, IEnumerable<string> scope = null)
 		{
 			var optional = new StringBuilder();
-			var state = "&state=" + System.Guid.NewGuid();
+			var stateQuery = "&state=" + state;
 
 			if (redirectUri != null) optional.Append("&redirect_uri=" + redirectUri);
 			if (scope != null) optional.Append("&scope=" + string.Join(",", scope));
 
-			return string.Format(@"{0}?client_id={1}{2}{3}", BaseUrl, clientId, optional, state);
+			return string.Format(@"{0}?client_id={1}{2}{3}", BaseUrl, clientId, optional, stateQuery);
 		}
 	}
 }
--- test/GitHubLabelInitialiser.Web.Test/Helpers/GitHubLoginHelperExtensionTest.cs
+++ test/GitHubLabelInitialiser.Web.Test/Helpers/GitHubLoginHelperExtensionTest.cs
@@ -54,13 +54,65 @@ namespace GitHubLabelInitialiser.Web.Test.Helpers
 			Assert.That(stateRemoved, Is.EqualTo(expectedWithoutState));
 		}
 
+		[Test]
+		public void LoginButton_WhenPassingInClientIdAndState_ThenReturnButtonHtml()
+		{
+			const string clientId = "wereteWHDSAWETW";
+			var state = RandomString();
+			var helper = MvcHelper.GetHtmlHelper();
+			var html = helper.LoginButton(clientId, state: state);
+			var expected = string.Format(
+					@"<a href=""https://github.com/login/oauth/authorize?client_id={0}&amp;state={1}"">Create account with GitHub</a>", clientId, state);
+
+			Assert.That(html.ToHtmlString(), Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void LoginButton_WhenPassingInClientIdAndRedirectUrlAndState_ThenReturnButtonHtml()
+		{
+			const string clientId = "wereteWHDSAWETW";
+			const string redirectUri = "http://localhost/callback/github/";
+			var state = RandomString();
+			var helper = MvcHelper.GetHtmlHelper();
+			var html = helper.LoginButton(clientId, redirectUri, state: state);
+			var expected =
+				string.Format(
+					@"<a href=""https://github.com/login/oauth/authorize?client_id={0}&amp;redirect_uri={1}&amp;state={2}"">Create account with GitHub</a>",
+					clientId, redirectUri, state);
+
+
+			Assert.That(html.ToHtmlString(), Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void LoginButton_WhenPassingInClientIdAndRedirectUrlAndScopeAndState_ThenReturnButtonHtml()
+		{
+			const string clientId = "wereteWHDSAWETW";
+			const string redirectUri = "http://localhost/callback/github/";
+			var state = RandomString();
+			var scope = new List<string> { "user", "user:email" };
+			var helper = MvcHelper.GetHtmlHelper();
+			var html = helper.LoginButton(clientId, redirectUri, scope, state);
+			var expected =
+				string.Format(
+					@"<a href=""https://github.com/login/oauth/authorize?client_id={0}&amp;redirect_uri={1}&amp;scope={2}&amp;state={3}"">Create account with GitHub</a>",
+					clientId, redirectUri, string.Join(",", scope), state);
+
+			Assert.That(html.ToHtmlString(), Is.EqualTo(expected));
+		}
+
+		private static string RandomString()
+		{
+			return System.Guid.NewGuid().ToString();
+		}
+
 		private static string RemoveState(string html)
 		{
 			return Regex.Replace(html, @"&amp;state=[^""]+", "");
 		}
 
 		[Test]
-		public void LoginButton_WhenCalling_ThenShouldHaveDifferentStateOnSubsequesntCalls()
+		public void LoginButton_WhenCalling_ThenShouldHaveDifferentStateOnSubsequentCalls()
 		{
 			const string clientId = "wereteWHDSAWETW";
 			const string redirectUri = "http://localhost/callback/github/";
