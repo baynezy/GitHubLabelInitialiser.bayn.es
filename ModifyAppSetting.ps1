7ac81136171e8adb41990b88b9de43237bb6dd94
Simon Baynes <baynezy@gmail.com>
2016-09-21 06:46:58 +0100

    Added Azure deploy config. Closes #7

---
+++ ModifyAppSetting.ps1
@@ -0,0 +1,6 @@
+param([string]$config, [string]$key, [string]$value)
+$doc = New-Object System.Xml.XmlDocument
+$doc.Load($config)
+$node = $doc.SelectSingleNode('configuration/appSettings/add[@key="' + $key + '"]')
+$node.Attributes['value'].Value = $value
+$doc.Save($config)
